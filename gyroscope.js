4c133a77b52424848bd854e3d061ed8b6e64a654
Aspa7beginner <65359456+Aspa7beginner@users.noreply.github.com>
2022-04-10 14:00:57 +0300

    add worker

---
+++ gyroscope.js
@@ -0,0 +1,42 @@
+let state = {active: false,
+            reading: 0,
+            logs: "Gyroscope -----------------"};
+
+let userStopRequest = 'false' || this.onmessage;
+
+navigator.permissions.query({name: 'gyroscope'})
+.then(function(result){
+    state.logs += "ingranted";
+    if(result.state === 'granted' && !this.onmessage){
+        try{
+            gyroscope = new Gyroscope({frequency: 1000});
+            gyroscope.addEventListener('reading', function(e){
+                state.logs += e;
+                state.reading = e;
+            });
+            gyroscope.start();
+            state.active = true;
+        }catch(error){
+                if (error.name === 'SecurityError') {
+                    state.logs+='Sensor construction was blocked by the Permissions Policy.<br>';
+                } else if (error.name === 'ReferenceError') {
+                    state.logs+='Sensor is not supported by the User Agent.<br>';
+                } else {
+                    state.logs+= "Other error: "+ error.name + "<br>";
+                }
+        }
+    }
+    else{
+        state.logs += "inerror";
+        if (error.name === 'NotAllowedError' || 'NotReadableError') {
+            state.logs +='Permission to access sensor was denied.<br> or ::--->  Cannot connect to the sensor.<br>';
+            alert('Permission to access sensor was denied');
+        } 
+        if (this.onmessage){
+            console.log(this.onmessage);
+            gyroscope.stop();
+        }
+    }
+});
+state.logs += "not at all in granted";
+this.postMessage(state);
